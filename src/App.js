adee1a4af77454b0b6c673e0e89f4359ab621609
iesub <70565881+iesub@users.noreply.github.com>
2022-03-21 12:06:10 +0300

    Решен вопрос авторизации

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import './css/App.css';
 
 function allo(){
   $.ajax({
-    url: 'http://192.168.1.192:13378/login',         
+    url: 'http://localhost:13378/login',         
     method: 'post',
     data: {username: "iesubbotin@gmail.com", password: "123"},             
     dataType: 'html',
-    credentials: "same-origin",          
+    credentials: "same-origin",
+    xhrFields:{
+      withCredentials: true
+    },               
     success: function(data){   
       $("#cont").append(data);           
     }
@@ -16,7 +19,7 @@ function allo(){
 
 function allo1(){
   $.ajax({
-    url: 'http://192.168.1.192:13378/',         
+    url: 'http://localhost:13378/',         
     method: 'get',
     data: {username: "iesubbotin@gmail.com", password: "123"},             
     dataType: 'html',          
